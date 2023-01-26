bd56a1d49dd44be6f2666776bba1f7aad0e91f0f
userzahar <110730909+userzahar@users.noreply.github.com>
2023-01-26 22:48:20 +0200

    рендер готовий?

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ let debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
 const searchInput = document.getElementById("search-box");
 const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector('.country-info')
 
 searchInput.addEventListener('input', debounce(countrySearch, DEBOUNCE_DELAY));
 
@@ -18,6 +19,8 @@ function searchCountryOnPromise(value) {
         .then(f => {
             if (f.length > 10) {
                 console.log("Too many matches found. Please enter a more specific name.")      
+            } else if (f.length === 1) {
+                renderOneCountryCard(f)
             } else {
                 console.log(f)
                 renderCountryList(f);  
@@ -29,5 +32,20 @@ function searchCountryOnPromise(value) {
 function renderCountryList(arrObj) {
     const markup = arrObj.map((el) => `<li class='country-list'><img src =${el.flags.svg} width='30' heigth='20'></img>${el.name.common}</li>`).join("");
     countryList.innerHTML = markup;
-    arrObj.map((el) => console.log(el.flags.svg))
 }
+
+function renderOneCountryCard(arrObj) {
+    countryList.innerHTML = '';
+    // const languages = arrObj.map((el) => {
+    //    el.languages.map(el=>console.log(el))
+    // })
+    const markupOne = arrObj.map((el) => `<h2><img src =${el.flags.svg} width='30' heigth='20'></img>${el.name.common}</h2><ul><li>Capital: ${el.capital}</li><li>Population: ${el.population}</li><li>Languages: ${Object.values(el.languages)}</li></ul>`).join("");
+    countryInfo.innerHTML = markupOne;
+    arrObj.map((el) => console.log(Object.values(el.languages)))
+}
+
+// function resetInput() {
+//     countryList.innerHTML = '';
+//     countryInfo.innerHTML = '';
+
+// }
